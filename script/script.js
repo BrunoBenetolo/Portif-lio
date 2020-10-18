3aec68bd5fd418c2e81034dec09855af17d964e2
BrunoBenetolo <brunobenetolo@hotmail.com>
2020-10-18 17:33:44 -0300

    :package: Conhecimentos

--- script/script.js
+++ script/script.js
@@ -1,6 +1,5 @@
 var hamburguer = document.querySelector(".hamburguer");
 
 hamburguer.addEventListener("click", function(){
-    console.warn(hamburguer);
     document.querySelector(".container").classList.toggle("show-menu");
 });
